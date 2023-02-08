ff717a68aa38ad739388e394ab17cb66048ea676
TMaiore <maiore.tristan@gmail.com>
2023-02-08 10:18:30 +0100

    redroom

---
+++ Assets/Scripts/Konami.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Konami : MonoBehaviour
+{
+
+    [SerializeField]
+    int haut = 0;
+    [SerializeField]
+    int bas = 0;
+    [SerializeField]
+    int gauche = 0;
+    [SerializeField]
+    int droite = 0;
+    [SerializeField]
+    int a = 0;
+    [SerializeField]
+    int b = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
